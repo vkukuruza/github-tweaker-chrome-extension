37d9bdaa3c0ea04a04281af6599087f6a1cb9567
Viktor Kukuruza <Viktor.Kukuruza@zilliant.com>
2023-09-18 20:28:46 +0300

    Fix onUpdate condition

--- src/background.js
+++ src/background.js
@@ -2,12 +2,9 @@
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   // this event fires multiple times (known bug), used notExistsSourceElement() method as workaround
-  if (changeInfo.status === "complete") {
+  if (changeInfo.status === "complete" && tab.title.startsWith('Pull requests · ')) {
     chrome.tabs.sendMessage(tabId, {
-      message: "tweak",
-      tabId: tabId,
-      changeInfo: changeInfo.url,
-      tab: tab.url,
+      message: "tweak"
     });
   }
 });
